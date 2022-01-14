2f380c5536dcce3a4538068cae38e0439aa46c75
soof-golan <83900570+soof-golan@users.noreply.github.com>
2022-01-14 14:20:01 +0200

    Update header to include author name

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,14 +1,13 @@
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import ErrorPage from 'next/error'
 import Container from '../../components/container'
 import PostBody from '../../components/post-body'
 import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
 import Layout from '../../components/layout'
-import { getPostBySlug, getAllPosts } from '../../lib/api'
+import {getAllPosts, getPostBySlug} from '../../lib/api'
 import PostTitle from '../../components/post-title'
 import Head from 'next/head'
-import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 import PostType from '../../types/post'
 
@@ -34,7 +33,7 @@ const Post = ({ post, morePosts, preview }: Props) => {
             <article className="mb-32">
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {post.title} | {post.author.name}
                 </title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
